9649eda6730e44feb5d2c505ca94622bbf1c5b95
aharisivakumar <aharisivakumar@gmail.com>
2026-07-10 03:55:21 +0530

    update the files

---
+++ index.js
@@ -0,0 +1,10 @@
+var sidebar=document.getElementById("sidenav")
+var closebar=document.getElementById("closenav")
+var menuicon=document.getElementById("menuicon")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+closebar.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
---
+++ products.js
@@ -0,0 +1,30 @@
+var sidebar=document.getElementById("sidenav")
+var closebar=document.getElementById("closenav")
+var menuicon=document.getElementById("menuicon")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+closebar.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+var productContainer=document.getElementById("product-container")
+var search=document.getElementById("search")
+var productlist=productContainer.querySelectorAll("div")
+
+    search.addEventListener("keyup",function(){
+      var enteredValue=event.target.value.toUpperCase()
+
+      for(count=0;count<productlist.length;count=count+1)
+      {
+        var productname=productlist[count].querySelector("p").textContent
+      if(productname.toUpperCase().indexOf(enteredValue)<0)
+      {
+        productlist[count].style.display="none"
+      }
+      else{
+        productlist[count].style.display="block"
+      }
+    }
+    })
